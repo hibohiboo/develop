d883773fc8f02037d0f8d79612dee2cdbe105c2b
hibohiboo <swdice66@gmail.com>
2016-11-03 20:49:26 +0900

    modify cmd

--- tutorial/lesson/identidock/cmd.sh
+++ tutorial/lesson/identidock/cmd.sh
@@ -4,7 +4,11 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Runnning Develop Server"
   exec python "identidock.py"
+elif [ "$ENV" = 'UNIT' ]; then
+  echo "Running Unit Tests"
+  exec python "tests.py"
 else
   echo "Runnning Production Server"
-  exec uwsgi --http 0.0.0.0:9000 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191
+  exec uwsgi --http 0.0.0.0:9000 --wsgi-file /app/identidock.py \
+             --callable app --stats 0.0.0.0:9191
 fi
