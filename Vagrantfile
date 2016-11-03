7f867bfb188298a632d57b0fc2dd76c9da35502a
hibohiboo <swdice66@gmail.com>
2016-11-03 15:01:11 +0900

    redis追加

--- Vagrantfile
+++ Vagrantfile
@@ -17,7 +17,8 @@ Vagrant.configure(2) do |config|
   config.vm.box_version = "2.2.9"
 
   # ネットワーク設定。
-  config.vm.network "private_network", ip: "192.168.50.10"
+  # 繋がらないときは/etc/network/interfaces を確認。enp0s8に設定してやる。
+  config.vm.network "private_network", ip: "192.168.50.10", auto_config:false
   # config.vm.network "forwarded_port", guest: 80, host: 3000
   # 共有するフォルダの設定
   # config.vm.synced_folder 'angular2', '/home/vagrant/angular2'
--- tutorial/lesson/identidock/app/identidock.py
+++ tutorial/lesson/identidock/app/identidock.py
@@ -1,8 +1,10 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import redis
 
 app = Flask(__name__)
+cache = redis.StrictRedis(host = 'redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
 default_name = 'Joe'
 
@@ -21,8 +23,8 @@ def mainpage():
               Hello <input type="text" name="name" value="{0}">
               <input type="submit" value="submit">
               </form>
-              <p> you look like a:
-              <img src="/monster/{1}" />
+              <p> you look like a:</p>
+              <img src="/monster/{1}"/>
               '''.format(name, name_hash)
     footer = '</body></html>'
 
@@ -30,8 +32,13 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-    r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
-    image = r.content
+
+    image = cache.get(name)
+    if image is None:
+        print ('Cache miss', flush=True)
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
+        image = r.content
+        cache.set(name, image)
 
     return Response(image, mimetype='image/png')
 
