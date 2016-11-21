98ecf7aa1b1293cf3ef7cc8c6a3d277bac9630a2
hibohiboo <swdice66@gmail.com>
2016-11-22 08:43:46 +0900

    サンプルjsx追加

--- tutorial/lesson/react/src/app.js
+++ tutorial/lesson/react/src/app.js
@@ -1,3 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sample from './warning.jsx';
+import sample from 'warning.jsx';
---
+++ tutorial/lesson/react/src/maibox.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Mailbox(props) {
+  // It works because in JavaScript, true && expression always evaluates to expression,
+  // and false && expression always evaluates to false
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  );
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+ReactDOM.render(
+  <Mailbox unreadMessages={messages} />,
+  document.getElementById('root')
+);
