af96e51c1033651a5bbb5526b0c0b8e291ca16a5
hibohiboo <swdice66@gmail.com>
2016-11-22 22:05:26 +0900

    textarea

--- tutorial/lesson/react/src/app.js
+++ tutorial/lesson/react/src/app.js
@@ -1,3 +1,3 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import sample from './form.jsx';
+import sample from './textarea.jsx';
---
+++ tutorial/lesson/react/src/textarea.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Please write an essay about your favorite DOM element.'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+ReactDOM.render(
+  <EssayForm />,
+  document.getElementById('root')
+);
