4f840161110d2bec079b016bf600521f425a2dda
hibohiboo <swdice66@gmail.com>
2016-12-03 12:57:10 +0900

    stateをconsoleで確認

--- tutorial/lesson/redux-todo/src/app.js
+++ tutorial/lesson/redux-todo/src/app.js
@@ -5,8 +5,23 @@ import { createStore } from 'redux'
 import todo from './reducers'
 import App from './components/App'
 
+import { addTodo } from './actions'
+
 // stateを保存するstoreを作成。
+// reducerとしてtodoを登録
 let store = createStore(todo)
+
+// actionCreatorであるaddTodo('Hello World!')で
+//{ id: 0, text: 'Hello World' }というactionを作成
+let helloAction = addTodo('Hello World!') 
+
+// store.dispatch関数にactionを渡す.
+// todo reducerに action = { id: 0, text: 'Hello World' },
+//                state = 現在のstate(undefined)
+store.dispatch(helloAction)
+
+// storeが保持しているstateをstore.getStateで取得
+console.log(store.getState()) // => Object {id: 0, text: "Hello World!"}
 
 render(
   // providerにstoreは必須
