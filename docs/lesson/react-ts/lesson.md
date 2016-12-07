e60cd88efb5ae2fee53dc117574fb3ec1dee2c10
hibohiboo <swdice66@gmail.com>
2016-12-07 22:10:06 +0900

    tsでhello world

---
+++ docs/lesson/react-ts/lesson.md
@@ -0,0 +1,13 @@
+# react + typescript tutorial
+
+## 参考
+
+[TypeScriptを使ってreactのチュートリアルを進めると捗るかなと思った。][*1]
+[React + TypeScript + Webpackの最小構成][*2]
+[npmでTypeScriptの型定義を管理できるtypesパッケージについて][*3]
+[TypeScript2.0での型定義ファイルの管理][*4]
+
+[*1]:http://qiita.com/m0a/items/d723259cdeebe382b5f6
+[*2]:http://qiita.com/uryyyyyyy/items/63969d6ed9341affdffb
+[*3]:http://qiita.com/laco0416/items/ed1aadf335f12cd3618d
+[*4]:http://sourcechord.hatenablog.com/entry/2016/09/28/005836
---
+++ tutorial/lesson/react-ts/webpack/webpack.config.babel.js
@@ -0,0 +1,58 @@
+import 'babel-polyfill'; // このファイルでes6の記法が使えるようにする
+import path from 'path';
+import webpack from 'webpack';
+
+module.exports = {
+  // src以下のソースをビルド対象とする
+  context: __dirname + '/src',
+  // エントリーポイントとしてapp.jsを起点にビルドする
+  entry: {
+    typescript: './app.tsx'
+  },
+  // distにビルドしたファイルをbundle.jsの名前で保存
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  // importするときに、以下の配列に登録した拡張子は省略できる
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
+  },
+  module: {
+    rules: [
+      // .js, .jsxに一致する拡張子のファイルはbabel-loaderを通してトランスパイル
+      { test: /\.jsx?$/,
+        exclude: /node_modules/,
+        // ビルド時に警告が出るのでcompact=falseを指定
+        loader: 'babel-loader?compact=false',
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.tsx?$/, exclude: /node_modules/, loader: "ts-loader" }
+    ]
+  },
+  // hot loadを有効にするためのプラグイン
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  // source-mapを出力して、ブラウザの開発者ツールからデバッグできるようにする。
+  devtool: '#cheap-module-eval-source-map',
+  // 開発サーバの設定
+  devServer: {
+    // public/index.htmlをデフォルトのホームとする
+    contentBase: './public',
+    // インラインモード
+    inline: true,
+    // 8080番ポートで起動
+    port: 8080,
+    // dockerのコンテナ上でサーバを動かすときは以下の設定で全ての接続を受け入れる
+    host:"0.0.0.0",
+    // hot loadを有効にする
+    hot: true
+  },
+  // vagrantの仕様でポーリングしないとファイルの変更を感知できない
+  watchOptions: {
+    aggregateTimeout: 300,
+    // ５秒毎にポーリング
+    poll: 5000
+  }
+};
