66cd14104c8a693346cbfb99c860b9566d72ca2d
hibohiboo <swdice66@gmail.com>
2016-12-10 10:03:43 +0900

    Todoをts化

--- tutorial/lesson/react-ts/src/components/Todo.tsx
+++ tutorial/lesson/react-ts/src/components/Todo.tsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
 import { PropTypes } from 'react';
 
-const Todo = ({ text }) => (
-  <li>
-    {text}
-  </li>
-);
+interface IProps extends React.Props<Todo> {
+    text: string;
+}
+
+class Todo extends React.Component<IProps, {}> {
+    render() {
+        return (
+          <li>
+            {this.props.text}
+          </li>
+        )
+    }
+}
 
 export default Todo;
