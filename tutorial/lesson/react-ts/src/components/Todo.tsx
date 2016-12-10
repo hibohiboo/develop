58d8aefb976b836f4271c2474c263678d9d8cc86
hibohiboo <swdice66@gmail.com>
2016-12-10 10:10:13 +0900

    コメント追記

--- tutorial/lesson/react-ts/src/components/Todo.tsx
+++ tutorial/lesson/react-ts/src/components/Todo.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { PropTypes } from 'react';
 
+// PropsをReact.Props<設定予定のコンポーネント>で継承して作ると補完が効く
+// パラメータが足りないとエラーを吐く
 interface IProps extends React.Props<Todo> {
     text: string;
 }
