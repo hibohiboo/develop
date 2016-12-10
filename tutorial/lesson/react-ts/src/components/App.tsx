53fec2e7282f27914a6769b2a766e62fb34b561d
hibohiboo <swdice66@gmail.com>
2016-12-10 09:33:17 +0900

    3. storeで保持したstateをViewで表示する（Js)

--- tutorial/lesson/react-ts/src/components/App.tsx
+++ tutorial/lesson/react-ts/src/components/App.tsx
@@ -1,9 +1,18 @@
 import * as React from 'react';
 
-class App extends React.Component<any, any> {
-    render() {
-        return <div> Hello World!!! </div>;
-    }
-}
+// class App extends React.Component<any, any> {
+//     render() {
+//         return <div> Hello World!!! </div>;
+//     }
+// }
 
-export default App;
+// export default App;
+
+import VisibleTodoList from '../containers/VisibleTodoList'
+
+const App = () => (
+  <div>
+    <VisibleTodoList />
+  </div>
+)
+export default App
