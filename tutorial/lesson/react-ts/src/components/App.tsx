32f3a208509c12c1d6a55fdc87360fd4c506d836
hibohiboo <swdice66@gmail.com>
2016-12-10 09:34:54 +0900

    App.tsxをtsに

--- tutorial/lesson/react-ts/src/components/App.tsx
+++ tutorial/lesson/react-ts/src/components/App.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-
-// class App extends React.Component<any, any> {
-//     render() {
-//         return <div> Hello World!!! </div>;
-//     }
-// }
-
-// export default App;
-
 import VisibleTodoList from '../containers/VisibleTodoList'
 
-const App = () => (
-  <div>
-    <VisibleTodoList />
-  </div>
-)
+class App extends React.Component<any, any> {
+    render() {
+        return (
+            <div>
+                <VisibleTodoList />
+            </div>            
+        );
+    }
+}
+
 export default App
