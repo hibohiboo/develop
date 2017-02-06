ffa051f6b0dc925ef2971008628d8187648d0a3c
hibohiboo <swdice66@gmail.com>
2017-02-06 22:00:26 +0900

    eslint fix テスト成功

--- tutorial/lesson/react-ts/src/components/App.tsx
+++ tutorial/lesson/react-ts/src/components/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import VisibleTodoList from '../containers/VisibleTodoList'
+import VisibleTodoList from '../containers/VisibleTodoList';
 import AddTodo from '../containers/AddTodo';
 import Footer from './Footer';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
