fd72c0393d6991bf9e6ff30e08c6825c9ba048d1
hibohiboo <swdice66@gmail.com>
2017-03-13 00:13:42 +0900

    セキュリティ対策

---
+++ docs/lesson/express/security.md
@@ -0,0 +1,7 @@
+# セキュリティ
+
+## 参考
+
+[チュートリアル][*1]  
+
+[*1]:http://expressjs.com/ja/advanced/best-practice-security.html
--- tutorial/lesson/express/myapp/app.js
+++ tutorial/lesson/express/myapp/app.js
@@ -4,11 +4,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var helmet = require('helmet');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// セキュリティ対策
+app.use(helmet());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
