f912e84866265edd9259b28fe88e3bca5ae67538
hibohiboo <swdice66@gmail.com>
2017-04-09 13:24:01 +0900

    add myproject

---
+++ tutorial/lesson/dotnet/dotnet/myproject/src/aspnetcoreapp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder() // builder パターン。アプリケーションホスト生成
+                .UseKestrel() // kestrel webサーバを使用
+                .UseUrls("http://0.0.0.0:5001") // dockerコンテナに外からアクセスするために0.0.0.0のアドレスで起動
+                .UseStartup<Startup>()
+                .Build();  // IWebHost オブジェクトを生成
+
+            host.Run(); // HTTP リクエストのリスニングを開始
+        }
+    }
+}
