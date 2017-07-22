bcd8586bf6e9f1facc2abe75ca83f9a67a6c4fa4
hibohiboo <swdice66@gmail.com>
2017-07-23 00:31:21 +0900

    一時保存

---
+++ bin/cd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# 現在の作業したいディレクトリに移動する。
+# http://d.hatena.ne.jp/kitokitoki/20120101/p2
+cd /vagrant/tutorial/lesson/learnjs/learnjs
+echo $$
+exec /bin/bash
---
+++ tutorial/lesson/learnJs/learnjs/public/app.js
@@ -0,0 +1,6 @@
+'use strict';
+var learnjs = {};
+learnjs.showView = function(hash){
+  var problemView = $('<div class="problem-view">').text('Coming soon!');
+  $('.view-container').empty().append(problemView);
+}
