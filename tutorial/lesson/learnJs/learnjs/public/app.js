2f74f647868a54eeadb7ea295ac6649a1338b660
hibohiboo <swdice66@gmail.com>
2017-07-23 21:22:55 +0900

    ランディングページビューの追加

--- tutorial/lesson/learnJs/learnjs/public/app.js
+++ tutorial/lesson/learnJs/learnjs/public/app.js
@@ -1,6 +1,15 @@
 'use strict';
 var learnjs = {};
+learnjs.problemView = ()=>{
+  return $('<div class="problem-view">').text('Coming soon!');
+};
 learnjs.showView = function(hash){
-  var problemView = $('<div class="problem-view">').text('Coming soon!');
-  $('.view-container').empty().append(problemView);
+  var routes = {
+    '#problem-1' : learnjs.problemView
+  };
+  var viewFn = routes[hash]; 
+  if(!viewFn){
+    return;
+  }
+  $('.view-container').empty().append(viewFn());
 }
--- tutorial/lesson/learnJs/learnjs/public/tests/app_spec.js
+++ tutorial/lesson/learnJs/learnjs/public/tests/app_spec.js
@@ -2,5 +2,10 @@ describe('LearnJs', function(){
   it('can show a problem view', function(){
     learnjs.showView('#problem-1');
     expect($('.view-container .problem-view').length).toEqual(1);
-  })
+  });
+  it('shows the landing page view when there is no hash', function(){
+    // デフォルトケース
+    learnjs.showView('');
+    expect($('.view-container .landing-view').length).toEqual(1);
+  });
 })
