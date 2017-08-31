ba31831b01652c993ea274daeb2082ea490da4e4
hibohiboo <swdice66@gmail.com>
2017-09-01 00:39:39 +0900

    selenium サンプル

--- tutorial/lesson/selenium/src/test.js
+++ tutorial/lesson/selenium/src/test.js
@@ -1,21 +1,17 @@
-'use strict';
-console.log('test');
-var webdriverio = require('webdriverio');
-
-var client = webdriverio
-.remote({ desiredCapabilities: { browserName: 'chrome' } })
-.init()
-.url('https://www.google.com/');
-
-console.log(client);
-
-client
-.getTitle()
-.then(function (title) {
-  console.log(title);
-})
-.getHTML('body')
-.then(function (data) {
-  console.log(data);
-})
-.end();
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const t = require('selenium-webdriver/testing');
+let driver = new Builder()
+.forBrowser('chrome')
+.usingServer('http://hub:4444/wd/hub')
+    .build();
+// https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/until.html
+driver.get('https://www.google.co.uk/').then(function () {
+  var query = driver.wait(until.elementLocated(By.name('q')));
+  query.sendKeys('webdriver\n');
+  driver.wait(until.titleContains('webdriver'), 1000).then((result)=>{
+      driver.findElement(By.css('#resultStats')).then(elm=>{
+        elm.getText().then(s=>console.log(s));
+      });
+  });
+  driver.quit();
+});
