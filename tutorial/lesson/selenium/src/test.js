1cff2f1dd034a5b15c1a24c21f1a91b4b977d590
hibohiboo <swdice66@gmail.com>
2017-09-01 00:45:08 +0900

    不要なファイルを削除

--- tutorial/lesson/selenium/src/test.js
+++ tutorial/lesson/selenium/src/test.js
@@ -1,3 +1,5 @@
+// http://qiita.com/teratsyk/items/bf63b308086cc1c0f828
+
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const t = require('selenium-webdriver/testing');
 let driver = new Builder()
@@ -5,7 +7,7 @@ let driver = new Builder()
 .usingServer('http://hub:4444/wd/hub')
     .build();
 // https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/until.html
-driver.get('https://www.google.co.uk/').then(function () {
+driver.get('https://www.google.com').then(function () {
   var query = driver.wait(until.elementLocated(By.name('q')));
   query.sendKeys('webdriver\n');
   driver.wait(until.titleContains('webdriver'), 1000).then((result)=>{
