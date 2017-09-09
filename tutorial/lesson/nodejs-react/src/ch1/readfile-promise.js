7061deab4395068017044ffabb43e94edc951758
hibohiboo <swdice66@gmail.com>
2017-09-10 00:25:28 +0900

    promiseのサンプル

---
+++ tutorial/lesson/nodejs-react/src/ch1/readfile-promise.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+/**
+ * Promiseを返す関数
+ */
+function readFile_pr(fname){
+  return new Promise((resolve)=>{
+    fs.readFile(fname, 'utf-8', (err, s)=>{
+      resolve(s)
+    })
+  })
+}
+
+// 順にテキストを読む
+readFile_pr('public/data/a.txt')
+.then((text)=>{
+  console.log('aを読みました', text)
+  return readFile_pr('public/data/b.txt')
+})
+.then((text)=>{
+  console.log('bを読みました', text)
+  return readFile_pr('public/data/c.txt')
+})
+.then((text)=>{
+  console.log('cを読みました', text)
+})
