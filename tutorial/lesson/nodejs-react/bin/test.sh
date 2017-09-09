7f5e609e6cf7ed9ce7170d230517eafb124ad2a6
hibohiboo <swdice66@gmail.com>
2017-09-10 00:01:26 +0900

    テスト実行用のbashを追加

---
+++ tutorial/lesson/nodejs-react/bin/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+command=${1:-"node src/generator-test.js"}
+
+# docker-composeの起動。bash実行
+cd $bin_dir/../ && docker-compose run node $command
