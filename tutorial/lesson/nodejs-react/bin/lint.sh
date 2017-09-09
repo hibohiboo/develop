b9b42e9f9c9fb590f876140264d2a9a614f999df
hibohiboo <swdice66@gmail.com>
2017-09-09 23:42:08 +0900

    standardのコーディング規約に修正

--- tutorial/lesson/nodejs-react/bin/lint.sh
+++ tutorial/lesson/nodejs-react/bin/lint.sh
@@ -3,5 +3,7 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 
+# --fixオプションをつけて実行で、修正を行う。 ex.) lint.sh --fix
+
 # docker-composeの起動。 コンテナ内に入る
-cd $bin_dir/../ && docker-compose run lint npm run -s eslint -- --ext .js src
+cd $bin_dir/../ && docker-compose run lint npm run -s eslint -- --ext .js src $@
--- tutorial/lesson/nodejs-react/src/App.js
+++ tutorial/lesson/nodejs-react/src/App.js
@@ -1,6 +1,6 @@
-const mm = m%60;
-const hh = Math.floor(mm/60);
-const z = (num)=>{
-  const s = "00" + String(num);
-  return s.substr(s.length-2, 2);
+const mm = m % 60
+const hh = Math.floor(mm / 60)
+const z = (num) => {
+  const s = '00' + String(num)
+  return s.substr(s.length - 2, 2)
 }
