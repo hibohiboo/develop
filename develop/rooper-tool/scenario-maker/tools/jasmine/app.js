43df2bfbea0ef3e672df2c1f80a50da385feb1d6
hibohiboo <swdice66@gmail.com>
2017-09-09 22:31:45 +0900

    一旦テストを削除

--- develop/rooper-tool/scenario-maker/tools/jasmine/app.js
+++ develop/rooper-tool/scenario-maker/tools/jasmine/app.js
@@ -13,7 +13,7 @@ const app = express();
 // });
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'public/tests')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
