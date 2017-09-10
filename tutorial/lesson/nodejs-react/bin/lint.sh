8338fc3f9b6711016c09ecc37a2f227286a68b0e
hibohiboo <swdice66@gmail.com>
2017-09-10 12:30:31 +0900

    react-appの雛形を追加。ホットリロードは機能せず

--- tutorial/lesson/nodejs-react/bin/lint.sh
+++ tutorial/lesson/nodejs-react/bin/lint.sh
@@ -3,7 +3,8 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 
-# --fixオプションをつけて実行で、修正を行う。 ex.) lint.sh --fix
-
+# オプション
+# lint.sh --fix
+# 修正できるものは修正する
 # docker-composeの起動。 コンテナ内に入る
-cd $bin_dir/../ && docker-compose run lint npm run -s eslint -- --ext .js src $@
+cd $bin_dir/../tools && docker-compose run lint npm run -s eslint -- --ext .js src $@
--- tutorial/lesson/nodejs-react/bin/up-d.sh
+++ tutorial/lesson/nodejs-react/bin/up-d.sh
@@ -4,4 +4,4 @@
 bin_dir=$(cd $(dirname $0) && pwd)
 
 # docker-composeのコンテナ作成
-cd $bin_dir/../ && docker-compose up -d --build
+cd $bin_dir/../tools && docker-compose up -d --build
