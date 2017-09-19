0695e92d80778c06561f958692a2823b2895eb41
hibohiboo <swdice66@gmail.com>
2017-09-19 22:40:34 +0900

    babel追加

---
+++ develop/rooper-tool/scenario-maker/docker/bin/babel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+command=${1:-"npm run babel"}
+
+# docker-composeの起動。 コンテナ内に入る
+cd $bin_dir/../tools && docker-compose run babel $command
