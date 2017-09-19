10f11cba27d0f548f56fab5f0be3a08cefbbfc17
hibohiboo <swdice66@gmail.com>
2017-09-20 01:54:34 +0900

    fix テストケース

--- develop/rooper-tool/scenario-maker/tests/e2e/test.js
+++ develop/rooper-tool/scenario-maker/tests/e2e/test.js
@@ -1,14 +1,14 @@
-'use strict';
+
 
 module.exports = {
-  'タイトルページ接続テスト': function (browser) {
-    //const {Builder, By, Key, until} = require('selenium-webdriver');
+  タイトルページ接続テスト(browser) {
+    // const {Builder, By, Key, until} = require('selenium-webdriver');
 
     browser
       .windowMaximize()
       .url(browser.globals.applicationUrl)
-      .assert.containsText('body','Tragedy Rooper Scenario Maker')
+      .assert.containsText('body', 'Tragedy Rooper Scenario Maker')
       .saveScreenshot('screenshots/test.png')
       .end();
-  }
+  },
 };
