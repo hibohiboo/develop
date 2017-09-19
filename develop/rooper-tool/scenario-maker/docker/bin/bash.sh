6dba2cb994374f1185658e8344692ca3039b5266
hibohiboo <swdice66@gmail.com>
2017-09-19 22:10:13 +0900

    dockerfileの位置を移動

--- develop/rooper-tool/scenario-maker/docker/bin/bash.sh
+++ develop/rooper-tool/scenario-maker/docker/bin/bash.sh
@@ -2,7 +2,7 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
-name=${1:-server}
+name=${1:-proxy}
 
 # docker-composeの起動。 コンテナ内に入る
 cd $bin_dir/../ && docker-compose run $name /bin/bash
