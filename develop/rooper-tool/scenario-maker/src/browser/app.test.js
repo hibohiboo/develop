f19a5f7f0150144372e589bc340b5274b8634f09
hibohiboo <swdice66@gmail.com>
2017-09-19 23:20:02 +0900

    eslintを追加

--- develop/rooper-tool/scenario-maker/src/browser/app.test.js
+++ develop/rooper-tool/scenario-maker/src/browser/app.test.js
@@ -1,6 +1,6 @@
-describe('ページ表示確認', ()=>{
-  it('ビューページが表示されること', ()=>{
+describe('ページ表示確認', () => {
+  it('ビューページが表示されること', () => {
     const root = document.getElementById('root');
     expect(root).toBeDefined();
-  })
-})
+  });
+});
