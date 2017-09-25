1f9fe0053ec916a41fc8db11803904962ba6e223
hibohiboo <swdice66@gmail.com>
2017-09-26 01:43:59 +0900

    sspa修正。デプロイテスト

--- develop/rooper-tool-ts/app/docker/bin/deploy.sh
+++ develop/rooper-tool-ts/app/docker/bin/deploy.sh
@@ -2,5 +2,9 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
+mkdir -p $bin_dir/../../dist/deploy/assets/js
+cp $bin_dir/../../public/index.html $bin_dir/../../dist/deploy/
+cp $bin_dir/../../dist/bundle-webpack/app.bundle.js $bin_dir/../../dist/deploy/assets/js/
+cp   $bin_dir/../../dist/bundle-webpack/vendor.bundle.js $bin_dir/../../dist/deploy/assets/js/
 
 $bin_dir/sspa deploy_bucket rooper.hibohiboo.com
