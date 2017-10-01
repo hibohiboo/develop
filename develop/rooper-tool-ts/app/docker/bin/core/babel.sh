ca934215bc83ae2d41c0b8cd8180ac572478be6e
hibohiboo <swdice66@gmail.com>
2017-10-01 21:05:54 +0900

    配置変更

--- develop/rooper-tool-ts/app/docker/bin/core/babel.sh
+++ develop/rooper-tool-ts/app/docker/bin/core/babel.sh
@@ -5,4 +5,4 @@ bin_dir=$(cd $(dirname $0) && pwd)
 command=${1:-"npm run babel"}
 
 # docker-composeの起動。 コンテナ内に入る
-cd $bin_dir/../tools && docker-compose run babel $command
+cd $bin_dir/../../tools/core && docker-compose run babel $command
