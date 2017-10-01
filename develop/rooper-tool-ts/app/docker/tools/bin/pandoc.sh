f0925de8816a6eade8597ea32ac14474e8696b53
hibohiboo <swdice66@gmail.com>
2017-10-01 22:16:24 +0900

    eslint

--- develop/rooper-tool-ts/app/docker/tools/bin/pandoc.sh
+++ develop/rooper-tool-ts/app/docker/tools/bin/pandoc.sh
@@ -3,4 +3,4 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 
-cd $bin_dir/../tools && docker-compose run webpack /bin/bash -c "npm run -s tslint -- -c tslint.json 'src/**/*.ts' $@"
+cd $bin_dir/.. && docker-compose run pandoc /bin/bash -c "for f in *.md; do pandoc \$f -s -o ../dist/\${f%.md}.html; done"
