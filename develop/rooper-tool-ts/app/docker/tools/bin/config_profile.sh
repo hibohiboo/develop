648275985588b9f95b0f324dd4aa7e4accd8798a
hibohiboo <swdice66@gmail.com>
2017-10-01 23:24:47 +0900

    sspa位置更新

--- develop/rooper-tool-ts/app/docker/tools/bin/config_profile.sh
+++ develop/rooper-tool-ts/app/docker/tools/bin/config_profile.sh
@@ -7,7 +7,7 @@ profile=${1:-admin}
 
 # docker-composeの起動
 function aws(){
-  COMPOSE_FILE=$bin_dir/../docker-compose.yml docker-compose run awscli /bin/bash -c "aws $@"
+  COMPOSE_FILE=$bin_dir/docker-compose.yml docker-compose run awscli /bin/bash -c "aws $@"
 }
 
 aws configure --profile $profile
