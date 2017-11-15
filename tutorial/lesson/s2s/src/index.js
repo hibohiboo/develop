b930408f521a8ae11254bf7a5f87bf3e3782bb55
hibohiboo <swdice66@gmail.com>
2017-11-16 01:20:22 +0900

    不要コメント削除

--- tutorial/lesson/s2s/src/index.js
+++ tutorial/lesson/s2s/src/index.js
@@ -2,9 +2,9 @@
  * @flow
  * Welcome to s2s https://github.com/akameco/s2s.
  * This example is very small. However Useful :)
- * This plugin is that initializing variable with flow information. test
+ * This plugin is that initializing variable with flow information. 
 */
 
 type User = { id: number, name: string }
-// type `var user: User` and save! test ttt
-var user: User = { id: 0, name: '' }
+// type `var user: User` and save!
+var user: User 
