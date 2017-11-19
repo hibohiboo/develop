cb339722ff804a074966cad80ce9f927fa22561b
hibohiboo <swdice66@gmail.com>
2017-11-20 02:22:31 +0900

    ブラウザで確認

---
+++ tutorial/lesson/s2s/docker/config/webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: '/app/dist'
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  },
+  performance: {
+    hints:false
+  },
+  devServer: {
+    // public/index.htmlをデフォルトのホームとする
+    contentBase: './public',
+    // インラインモード
+    inline: true,
+    // 8080番ポートで起動
+    port: 8080,
+    // dockerのコンテナ上でサーバを動かすときは以下の設定で全ての接続を受け入れる
+    host:"0.0.0.0",
+    // hot loadを有効にする
+   // hot: true,
+    // ログレベルをinfoに
+    //clientLogLevel: "info",
+  },
+  // vagrantの仕様でポーリングしないとファイルの変更を感知できない
+  watchOptions: {
+    aggregateTimeout: 300,
+    // ５秒毎にポーリング
+    poll: 5000
+  }
+};
