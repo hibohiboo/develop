c596fde04f943f7f0161df76b985d88080200a9b
hibohiboo <swdice66@gmail.com>
2017-11-25 01:56:23 +0900

    initialstate追加

--- tutorial/lesson/s2s-saga/src/reducers/coin.js
+++ tutorial/lesson/s2s-saga/src/reducers/coin.js
@@ -1,7 +1,25 @@
 import { handleActions } from "redux-actions";
 import * as actions from "../actions";
 
-const initialState = {};
+const initialState = {
+  coin: {
+    id: "",
+    name: "",
+    symbol: "",
+    rank: "",
+    price_usd: "",
+    price_btc: "",
+    "24h_volume_usd": "",
+    market_cap_usd: "",
+    available_supply: "",
+    total_supply: "",
+    max_supply: "",
+    percent_change_1h: "",
+    percent_change_24h: "",
+    percent_change_7d: "",
+    last_updated: ""
+  }
+};
 
 export default handleActions(
   {
