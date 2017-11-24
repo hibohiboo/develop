8815d6d0bb8b6b1b909b14861456c95f8e2e8b29
hibohiboo <swdice66@gmail.com>
2017-11-25 01:55:32 +0900

    アクションの追加

--- tutorial/lesson/s2s-saga/src/reducers/coin.js
+++ tutorial/lesson/s2s-saga/src/reducers/coin.js
@@ -1,9 +1,19 @@
-import { handleActions } from 'redux-actions'
-import * as actions from '../actions'
+import { handleActions } from "redux-actions";
+import * as actions from "../actions";
 
-const initialState = {}
+const initialState = {};
 
 export default handleActions(
-  {},
+  {
+    [actions.getCoinRequest]: (state, action) => ({
+      ...state
+    }),
+    [actions.getCoinSuccess]: (state, action) => ({
+      ...state
+    }),
+    [actions.getCoinFailure]: (state, action) => ({
+      ...state
+    })
+  },
   initialState
-)
+);
