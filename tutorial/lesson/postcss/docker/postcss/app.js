d0d8bed7b75ea5c9ab6f0b3a878b43ee16a4be67
hibohiboo <swdice66@gmail.com>
2017-12-23 00:50:06 +0900

    PostCss用にVSCode設定

--- tutorial/lesson/postcss/docker/postcss/app.js
+++ tutorial/lesson/postcss/docker/postcss/app.js
@@ -5,13 +5,16 @@ const nested = require('postcss-nested');
 const autoprefixer = require('autoprefixer');
 const cssnext = require('postcss-cssnext');
 
-fs.readFile('src/app.css', (err, css) => {
+const target = 'src/app.pcss'
+
+
+fs.readFile(target, (err, css) => {
 postcss([   cssnext({
     features: {
       customProperties: false
     }
   })])
-    .process(css, { from: 'src/app.css', to: 'dest/app.css', parser: comment })
+    .process(css, { from: target, to: 'dest/app.css', parser: comment })
     .then(result => {
         fs.writeFile('dest/app.css', result.css, (err)=>console.log(err));
         if ( result.map ) fs.writeFile('dest/app.css.map', result.map, (err)=>console.log(err));
