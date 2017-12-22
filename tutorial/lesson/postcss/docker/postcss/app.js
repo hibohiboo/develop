396c164bbebc68d014f9f2555a47aa8148e78c30
hibohiboo <swdice66@gmail.com>
2017-12-23 01:09:51 +0900

    chokidarによる監視

--- tutorial/lesson/postcss/docker/postcss/app.js
+++ tutorial/lesson/postcss/docker/postcss/app.js
@@ -4,19 +4,39 @@ const comment = require('postcss-comment');
 const nested = require('postcss-nested');
 const autoprefixer = require('autoprefixer');
 const cssnext = require('postcss-cssnext');
+const chokidar = require('chokidar');
+//const target = 'src/app.pcss'
 
-const target = 'src/app.pcss'
+chokidar.watch('/app/src/*', {
+  ignored: /(^|[\/\\])\../,
+  persistent: true,
+  // followSymlinks: false,
+  // useFsEvents: false,
+  usePolling: true
+}).on('all', function(event, path) {
+  console.log(event, path);
+  transpile(path);
+}).on('ready', function() {
+  console.log('Ready');
+})
 
+/**
+ * PostCssを使ってpcssをトランスパイルする。
+ * 
+ */
+function transpile(target){
+  console.log('target:' + target);
 
-fs.readFile(target, (err, css) => {
-postcss([   cssnext({
-    features: {
-      customProperties: false
-    }
-  })])
-    .process(css, { from: target, to: 'dest/app.css', parser: comment })
-    .then(result => {
-        fs.writeFile('dest/app.css', result.css, (err)=>console.log(err));
-        if ( result.map ) fs.writeFile('dest/app.css.map', result.map, (err)=>console.log(err));
+  fs.readFile(target, (err, css) => {
+    postcss([   cssnext({
+        features: {
+          customProperties: false
+        }
+      })])
+        .process(css, { from: target, to: 'dest/app.css', parser: comment })
+        .then(result => {
+            fs.writeFile('dest/app.css', result.css, (err)=>console.log(err));
+            if ( result.map ) fs.writeFile('dest/app.css.map', result.map, (err)=>console.log(err));
+        });
     });
-});
+}
