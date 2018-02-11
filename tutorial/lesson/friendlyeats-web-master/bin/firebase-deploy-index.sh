2256bb9fbf9ca3f3322cc8b2318a608f3590b814
hibohiboo <swdice66@gmail.com>
2018-02-11 10:22:03 +0900

    backgroundでdockerが起動していてもデプロイできるように修正

--- tutorial/lesson/friendlyeats-web-master/bin/firebase-deploy-index.sh
+++ tutorial/lesson/friendlyeats-web-master/bin/firebase-deploy-index.sh
@@ -2,20 +2,18 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
-
 name=${1:-firebase}
-
 # $container_nameの有無をgrepで調べる
 docker ps | grep $name
 
 # grepの戻り値$?の評価。 grep戻り値 0:一致した 1:一致しなかった
 if [ $? -eq 0 ]; then
   # 一致したときの処理
-  cd $bin_dir/../docker && docker-compose exec $name firebase deploy --only firestore:rules --token "$FIREBASE_TOKEN"
+  cd $bin_dir/../docker && docker-compose exec $name firebase deploy --only firestore:indexes --token "$FIREBASE_TOKEN"
 else
   # 一致しなかった時の処理
   # コンテナを立ち上げて接続
   # docker-composeの起動。 OAuth用に9005. サンプルアプリ用に5000ポートを開放。
-  cd $bin_dir/../docker && docker-compose run -p 9005:9005 -p 5000:5000 $name firebase deploy --only firestore:rules --token "$FIREBASE_TOKEN"
+  cd $bin_dir/../docker && docker-compose run -p 9005:9005 -p 5000:5000 $name firebase deploy --only firestore:indexes --token "$FIREBASE_TOKEN"
 fi
 
--- tutorial/lesson/friendlyeats-web-master/scripts/FriendlyEats.Data.js
+++ tutorial/lesson/friendlyeats-web-master/scripts/FriendlyEats.Data.js
@@ -21,27 +21,51 @@ FriendlyEats.prototype.addRestaurant = function(data) {
 };
 
 FriendlyEats.prototype.getAllRestaurants = function(render) {
-  /*
-    TODO: Retrieve list of restaurants
-  */
+  const query = firebase.firestore()
+  .collection('restaurants')
+  .orderBy('avgRating', 'desc')
+  .limit(50);
+  this.getDocumentsInQuery(query, render);
 };
 
 FriendlyEats.prototype.getDocumentsInQuery = function(query, render) {
-  /*
-    TODO: Render all documents in the provided query
-  */
+  query.onSnapshot(snapshot => {
+    if (!snapshot.size) return render();
+ 
+    snapshot.docChanges.forEach(change => {
+      if (change.type === 'added') {
+        render(change.doc);
+      }
+    });
+  });
 };
 
 FriendlyEats.prototype.getRestaurant = function(id) {
-  /*
-    TODO: Retrieve a single restaurant
-  */
+  return firebase.firestore().collection('restaurants').doc(id).get();
 };
 
 FriendlyEats.prototype.getFilteredRestaurants = function(filters, render) {
-  /*
-    TODO: Retrieve filtered list of restaurants
-  */
+  let query = firebase.firestore().collection('restaurants');
+
+  if (filters.category !== 'Any') {
+    query = query.where('category', '==', filters.category);
+  }
+  
+  if (filters.city !== 'Any') {
+    query = query.where('city', '==', filters.city);
+  }
+  
+  if (filters.price !== 'Any') {
+    query = query.where('price', '==', filters.price.length);
+  }
+  
+  if (filters.sort === 'Rating') {
+    query = query.orderBy('avgRating', 'desc');
+  } else if (filters.sort === 'Reviews') {
+    query = query.orderBy('numRatings', 'desc');
+  }
+  
+  this.getDocumentsInQuery(query, render);
 };
 
 FriendlyEats.prototype.addRating = function(restaurantID, rating) {
