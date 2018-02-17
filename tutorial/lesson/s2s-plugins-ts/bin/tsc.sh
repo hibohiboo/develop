b910acebb5935cc01d9db7baa598ed7eeb268e12
hibohiboo <swdice66@gmail.com>
2018-02-17 15:32:20 +0900

    一時保存

--- tutorial/lesson/s2s-plugins-ts/bin/tsc.sh
+++ tutorial/lesson/s2s-plugins-ts/bin/tsc.sh
@@ -3,4 +3,4 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 # tslint.sh --fix オプションで自動修正
-cd $bin_dir/../docker && docker-compose run webpack yarn run tsc 
+cd $bin_dir/../docker && docker-compose run ts yarn run tsc 
--- tutorial/lesson/s2s-plugins-ts/bin/tslint.sh
+++ tutorial/lesson/s2s-plugins-ts/bin/tslint.sh
@@ -3,4 +3,4 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 # tslint.sh --fix オプションで自動修正
-cd $bin_dir/../docker && docker-compose run tslint npm run -s tslint -- -c tslint.json src/app.tsx src/**/*.ts src/**/*.tsx $@
+cd $bin_dir/../docker && docker-compose run ts npm run -s tslint -- -c tslint.json src/app.tsx src/**/*.ts src/**/*.tsx $@
