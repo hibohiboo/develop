2418007e860778e85a7bd68a7f2f524083228671
hibohiboo <swdice66@gmail.com>
2018-02-17 17:35:20 +0900

    引数から取得するように変更

--- tutorial/lesson/s2s-plugins-ts/packages/babel-plugin-test/index.js
+++ tutorial/lesson/s2s-plugins-ts/packages/babel-plugin-test/index.js
@@ -1,18 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var t = require("babel-types");
 /**
  *
  * @param param0 {types:Types} tはbabel-types
  */
-var plugin = function () { return ({
-    visitor: {
-        BinaryExpression: function (nodePath) {
-            if (nodePath.node.operator !== '*') {
-                var newAst = t.binaryExpression('*', nodePath.node.left, nodePath.node.right);
-                nodePath.replaceWith(newAst);
+var plugin = function (args) {
+    var t = args && args.types;
+    return {
+        visitor: {
+            BinaryExpression: function (nodePath) {
+                if (nodePath.node.operator !== '*') {
+                    var newAst = t.binaryExpression('*', nodePath.node.left, nodePath.node.right);
+                    nodePath.replaceWith(newAst);
+                }
             }
         }
-    }
-}); };
+    };
+};
 exports.default = plugin;
--- tutorial/lesson/s2s-plugins-ts/src/babel-plugin-test/index.ts
+++ tutorial/lesson/s2s-plugins-ts/src/babel-plugin-test/index.ts
@@ -1,21 +1,23 @@
 import {transform, PluginObj} from 'babel-core';
 import {NodePath} from 'babel-traverse';
 import {BinaryExpression} from 'babel-types';
-import  * as t from 'babel-types';
+import  PluginArgs from '../PluginArgs.d';
 
 /**
  * 
  * @param param0 {types:Types} tはbabel-types
  */
-const plugin = () =>({
-  visitor:{
-    BinaryExpression: (nodePath: NodePath<BinaryExpression>) =>{
-      if(nodePath.node.operator !== '*'){
-        const newAst = t.binaryExpression('*', nodePath.node.left, nodePath.node.right);
-        nodePath.replaceWith(newAst);
+const plugin = (args:PluginArgs):PluginObj => {
+  const t = args && args.types;
+  return {
+    visitor:{
+      BinaryExpression: (nodePath: NodePath<BinaryExpression>) =>{
+        if(nodePath.node.operator !== '*'){
+          const newAst = t.binaryExpression('*', nodePath.node.left, nodePath.node.right);
+          nodePath.replaceWith(newAst);
+        }
       }
     }
-  }
-});
+}};
 
 export default plugin;
