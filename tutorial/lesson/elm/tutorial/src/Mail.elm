a7fedf5ec8f9f6353c8cc69afdaa4f6466fdbb91
hibohiboo <swdice66@gmail.com>
2018-02-27 08:23:58 +0900

    チュートリアル

---
+++ tutorial/lesson/elm/tutorial/src/Mail.elm
@@ -0,0 +1,66 @@
+module Main exposing (..)
+
+import Html exposing (Html, div, text, program)
+
+-- モデル
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello", Cmd.none )
+
+
+
+-- メッセージ
+
+
+type Msg
+    = NoOp
+
+
+
+-- ビュー
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text model ]
+
+
+
+-- 更新
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- サブスクリプション(購読)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- MAIN
+
+
+main : Program Never Model Msg
+main =
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
