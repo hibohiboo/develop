c0740dab9162c7393d2997ad7e9b85bdd993dbb5
hibohiboo <swdice66@gmail.com>
2018-02-27 23:45:59 +0900

    バックエンドを試す

--- tutorial/lesson/elm/bin/bash.sh
+++ tutorial/lesson/elm/bin/bash.sh
@@ -2,7 +2,7 @@
 
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
-name=${1:-elm}
+name=${1:-elm-dev}
 
 # docker-composeの起動。 コンテナ内に入る. 
 cd $bin_dir/../docker && docker-compose run  $name /bin/bash 
--- tutorial/lesson/elm/tutorial/webpack.config.js
+++ tutorial/lesson/elm/tutorial/webpack.config.js
@@ -44,10 +44,21 @@ module.exports = {
     noParse: /\.elm$/,
   },
 
+  // 開発サーバの設定
   devServer: {
+    // インラインモード
     inline: true,
     stats: { colors: true },
+    // 3000番ポートで起動
+    port: 3000,
+    // dockerのコンテナ上でサーバを動かすときは以下の設定で全ての接続を受け入れる
+    host:"0.0.0.0",
   },
-
+  // vagrantの仕様でポーリングしないとファイルの変更を感知できない
+  watchOptions: {
+    aggregateTimeout: 300,
+    // ５秒毎にポーリング
+    poll: 5000
+  }
 
 };
