31147b75555e4b1f44727064812505e34f367a99
hibohiboo <swdice66@gmail.com>
2018-03-29 01:43:36 +0900

    セレニウムで、npm install chromedriverに失敗したのでapkでダウンロードに変更

--- tutorial/lesson/selenium/bin/run.sh
+++ tutorial/lesson/selenium/bin/run.sh
@@ -4,4 +4,4 @@
 bin_dir=$(cd $(dirname $0) && pwd)
 name=${1:-alpine}
 # docker-composeの起動。 コンテナ内に入る4
-cd $bin_dir/../ && docker-compose run $name /bin/bash
+cd $bin_dir/../ && docker-compose run $name node src/test.js
--- tutorial/lesson/selenium/src/test.js
+++ tutorial/lesson/selenium/src/test.js
@@ -3,17 +3,18 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 const t = require('selenium-webdriver/testing');
 let driver = new Builder()
-.forBrowser('chrome')
-.usingServer('http://hub:4444/wd/hub')
-    .build();
+  .forBrowser('chrome')
+  .usingServer('http://hub:4444/wd/hub')
+  .build();
 // https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/until.html
 driver.get('https://www.google.com').then(function () {
   var query = driver.wait(until.elementLocated(By.name('q')));
   query.sendKeys('webdriver\n');
-  driver.wait(until.titleContains('webdriver'), 1000).then((result)=>{
-      driver.findElement(By.css('#resultStats')).then(elm=>{
-        elm.getText().then(s=>console.log(s));
-      });
-  });
-  driver.quit();
+  driver.wait(until.titleContains('webdriver'), 1000)
+        .then((result)=>{
+          driver.findElement(By.css('#resultStats')).then(elm=>{
+            elm.getText().then(s=>console.log(s));
+            driver.quit();
+          });
+        });
 });
