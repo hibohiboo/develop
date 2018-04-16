da2774193c4b0625356532da386487810a778573
hibohiboo <swdice66@gmail.com>
2018-04-16 19:56:26 +0900

    一時保存

--- tutorial/lesson/elm/tutorial/src/Main.elm
+++ tutorial/lesson/elm/tutorial/src/Main.elm
@@ -16,46 +16,30 @@ init =
 
 
 -- メッセージ
-
-
-type Msg
-    = NoOp
+type Msg = NoOp
 
 
 
 -- ビュー
-
-
 view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
+view model = div [] [ text model ]
 
 
 
 -- 更新
-
-
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
             ( model, Cmd.none )
 
-
-
 -- サブスクリプション(購読)
-
-
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+subscriptions model = Sub.none
 
 
 
 -- MAIN
-
-
 main : Program Never Model Msg
 main =
     program
