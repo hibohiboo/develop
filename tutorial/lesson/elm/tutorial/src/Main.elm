e343d7f0d33d9b5630d13cf2511b7124873f7ec8
hibohiboo <swdice66@gmail.com>
2018-04-19 01:30:07 +0900

    widget

--- tutorial/lesson/elm/tutorial/src/Main.elm
+++ tutorial/lesson/elm/tutorial/src/Main.elm
@@ -1,64 +1,73 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text, program)
-import Html.Events exposing (onClick)
-
+import Html exposing (Html, program)
+import Widget
 
 -- モデル
 
 
-type alias Model =
-    Int
+type alias AppModel =
+    { widgetModel : Widget.Model
+    }
 
 
-init : ( Model, Cmd Msg )
+initialModel : AppModel
+initialModel =
+    { widgetModel = Widget.initialModel
+    }
+
+
+init : ( AppModel, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( initialModel, Cmd.none )
+
 
 
 -- メッセージ
 
 
 type Msg
-    = Increment Int
+    = WidgetMsg Widget.Msg
 
 
 
--- ビュー
+-- VIEW
 
 
-view : Model -> Html Msg
+view : AppModel -> Html Msg
 view model =
-    div []
-        [ button [ onClick (Increment 2) ] [ text "+" ]
-        , text (toString model)
+    Html.div []
+        [ Html.map WidgetMsg (Widget.view model.widgetModel)
         ]
 
 
 -- 更新
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increment howMuch ->
-            ( model + howMuch, Cmd.none )
-
+update : Msg -> AppModel -> ( AppModel, Cmd Msg )
+update message model =
+    case message of
+        WidgetMsg subMsg ->
+            let
+                ( updatedWidgetModel, widgetCmd ) =
+                    Widget.update subMsg model.widgetModel
+            in
+                ( { model | widgetModel = updatedWidgetModel }, Cmd.map WidgetMsg widgetCmd )
 
 
 -- サブスクリプション(購読)
 
 
-subscriptions : Model -> Sub Msg
+subscriptions : AppModel -> Sub Msg
 subscriptions model =
     Sub.none
 
 
 
--- MAIN
+-- APP
 
 
-main : Program Never Model Msg
+main : Program Never AppModel Msg
 main =
     program
         { init = init
