bbdc1654d1432b3b54784031d253b63cb43acea3
hibohiboo <swdice66@gmail.com>
2018-04-29 00:21:59 +0900

    qiita tutorial 1

--- tutorial/lesson/elm/docs/readme.md
+++ tutorial/lesson/elm/docs/readme.md
@@ -9,6 +9,8 @@
 
 [Elm チュートリアル][*1]
 [シンタックス][*2]
+[qiita入門elm][*3]
 
 [*1]:https://www.elm-tutorial.org/jp/02-elm-arch/01-introduction.html
 [*2]:http://gtech.hatenablog.com/entry/2016/10/21/110212
+[*3]:https://qiita.com/mather314/items/e9e08960d91b187652e5
---
+++ tutorial/lesson/elm/tutorial/src2/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// index.htmlがdistにコピーされるようにRequireする
+require('./index.html');
+
+var Elm = require('./Main.elm');
+var mountNode = document.getElementById('main');
+
+// .embed()はオプションの第二引数を取り、プログラム開始に必要なデータを与えられる。たとえばuserIDや何らかのトークンなど
+var app = Elm.Main.embed(mountNode);
