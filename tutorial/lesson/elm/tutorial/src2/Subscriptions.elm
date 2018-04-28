be29db7a6232bb304c57901dd2a19a2d4ad356c7
hibohiboo <swdice66@gmail.com>
2018-04-29 03:29:12 +0900

    日付更新

--- tutorial/lesson/elm/tutorial/src2/Subscriptions.elm
+++ tutorial/lesson/elm/tutorial/src2/Subscriptions.elm
@@ -1,8 +1,14 @@
 module Subscriptions exposing(..)
 
+import Date exposing (Date)
+import Time exposing (Time)
 import Msgs exposing(..)
 import Models exposing (Model)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    let
+        updateCurrentDate t = 
+            Date.fromTime t |> UpdateDatetime
+    in
+        Time.every Time.second updateCurrentDate
