c313b61a0e06e5f18753996ac43d14ba9b2588f7
hibohiboo <swdice66@gmail.com>
2018-06-21 01:27:54 +0900

    minikubeがあまりうまく動かない

--- provision/playbooks/roles/minikube/files/install.sh
+++ provision/playbooks/roles/minikube/files/install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
-cd /usr/local/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl
+# https://qiita.com/mumoshu/items/8f55ee830d8e5c172dd4
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+#curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
@@ -12,15 +11,15 @@ mkdir -p $HOME/.kube
 touch $HOME/.kube/config
 
 export KUBECONFIG=$HOME/.kube/config
-sudo -E ./minikube start --vm-driver=none
+sudo -E minikube start --vm-driver=none
 
-# this for loop waits until kubectl can access the api server that Minikube has created
-for i in {1..150}; do # timeout for 5 minutes
-   ./kubectl get po &> /dev/null
-   if [ $? -ne 1 ]; then
-      break
-  fi
-  sleep 2
-done
+# this for loop waits until kubectl can access tmihe api server that Minikube has created
+# for i in {1..150}; do # timeout for 5 minutes
+#    ./kubectl get po &> /dev/null
+#    if [ $? -ne 1 ]; then
+#       break
+#   fi
+#   sleep 2
+# done
 
 # kubectl commands are now able to interact with Minikube cluster
