fe81d72c824fe9913b3590734bd600339d8edf84
hibohiboo <swdice66@gmail.com>
2018-06-25 23:54:28 +0900

    シンボリックリンクを実体としてコピー

--- tutorial/lesson/elm-doc/bin/elm-doc.sh
+++ tutorial/lesson/elm-doc/bin/elm-doc.sh
@@ -6,6 +6,6 @@ composeFile=${1:-"docker-compose.yml"}
 containerName=${2:-"elm-doc"}
 
 # docker-composeの起動
-cd $bin_dir/../docker && docker-compose -f $composeFile run $containerName /bin/bash -c "elm-doc . --output docs"
+cd $bin_dir/../docker && docker-compose -f $composeFile run $containerName /bin/bash -c "elm-doc . --output docs && cp -Lr /app/docs /app/api"
 # docker cp /path/to/my-local-file.sql "$(docker-compose ps -q mycontainer)":/file-on-container.sql
-docker cp ` cd $bin_dir/../docker && docker-compose ps -q $containerName`:/app/docs /vagrant/tutorial/lesson/elm-doc/docs/api/
+# docker cp ` cd $bin_dir/../docker && docker-compose ps -q $containerName`:/app/docs /vagrant/tutorial/lesson/elm-doc/docs/api/
