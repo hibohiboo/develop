08abe5bc0936f43721cc53eff7bc9a6b70f3b1df
hibohiboo <swdice66@gmail.com>
2018-06-25 23:45:51 +0900

    コピーに失敗

--- tutorial/lesson/elm-doc/bin/elm-doc.sh
+++ tutorial/lesson/elm-doc/bin/elm-doc.sh
@@ -7,3 +7,5 @@ containerName=${2:-"elm-doc"}
 
 # docker-composeの起動
 cd $bin_dir/../docker && docker-compose -f $composeFile run $containerName /bin/bash -c "elm-doc . --output docs"
+# docker cp /path/to/my-local-file.sql "$(docker-compose ps -q mycontainer)":/file-on-container.sql
+docker cp ` cd $bin_dir/../docker && docker-compose ps -q $containerName`:/app/docs /vagrant/tutorial/lesson/elm-doc/docs/api/
