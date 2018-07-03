760b54b907e7353cce5943785b23bbdd1db425d9
hibohiboo <swdice66@gmail.com>
2018-07-04 07:41:13 +0900

    一時保存

---
+++ tutorial/lesson/php/lumen-5.4/bin/composer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 変数設定
+bin_dir=$(cd $(dirname $0) && pwd)
+composeFile=${1:-"docker-compose.yml"}
+containerName=${2:-"composer"}
+
+# docker-composeの起動
+cd $bin_dir/../docker && docker-compose -f $composeFile run $containerName /bin/bash -c "composer require laravel/lumen"
