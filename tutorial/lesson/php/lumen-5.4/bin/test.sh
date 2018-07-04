9c568ccb0ab7faf59260ea566ad00a98281b4de1
hibohiboo <swdice66@gmail.com>
2018-07-04 22:50:17 +0900

    lumen起動テスト

--- tutorial/lesson/php/lumen-5.4/bin/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# このシェルスクリプトのディレクトリの絶対パスを取得。
-bin_dir=$(cd $(dirname $0) && pwd)
-
-# docker-composeの起動。 コンテナ内に入る. 
-cd $bin_dir/../docker && docker-compose run elm-dev npm run -s elm-test
---
+++ tutorial/lesson/php/lumen/src/index.php
@@ -0,0 +1,3 @@
+<?php
+
+require 'blog/public/index.php';
