534d47d5ecfbf43cef44c1933f9e348591012289
hibohiboo <swdice66@gmail.com>
2018-07-12 01:08:50 +0900

    環境変数を読み込むように修正

--- tutorial/lesson/spring/bin/sqlplus.sh
+++ tutorial/lesson/spring/bin/sqlplus.sh
@@ -4,13 +4,16 @@
 bin_dir=$(cd $(dirname $0) && pwd)
 container_name=${1:-database}
 
+# 環境変数読み込み
+. $bin_dir/../docker/.env
+
 # $container_nameの有無をgrepで調べる
 docker ps | grep $container_name
 
 # grepの戻り値$?の評価。 grep戻り値 0:一致した 1:一致しなかった
 if [ $? -eq 0 ]; then
   # 一致したときの処理
-  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/oraoraora@localhost:1521/XE as sysdba
+  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/$ORACLE_PWD@localhost:1521/XE as sysdba
 else
   # 一致しなかった時の処理
   # コンテナを立ち上げて接続
@@ -24,6 +27,6 @@ else
       && echo "wait 1 seconds"  && sleep 1
   echo "データベース立ち上げ完了。Enterキーを2回押してください。その後、シェルをもう一度実行してください。"
   echo ">SQL の入力画面にならなかった場合は、もう数秒待って再度シェルを実行してください。"
-  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/oraoraora@localhost:1521/XE as sysdba  > /dev/null 2>&1
+  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/$ORACLE_PWD@localhost:1521/XE as sysdba  > /dev/null 2>&1
 
 fi
