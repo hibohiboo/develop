fcd709a7396d01d6a5cd86f74cd4748c4d9795bb
hibohiboo <swdice66@gmail.com>
2018-07-12 01:03:37 +0900

    readme追記

---
+++ tutorial/lesson/spring/bin/sqlplus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+container_name=${1:-database}
+
+# $container_nameの有無をgrepで調べる
+docker ps | grep $container_name
+
+# grepの戻り値$?の評価。 grep戻り値 0:一致した 1:一致しなかった
+if [ $? -eq 0 ]; then
+  # 一致したときの処理
+  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/oraoraora@localhost:1521/XE as sysdba
+else
+  # 一致しなかった時の処理
+  # コンテナを立ち上げて接続
+  cd $bin_dir/../docker && docker-compose up -d \
+      && echo "データベースを立ち上げ中。15秒ほどお待ちください。。"  && sleep 2 \
+      && echo "wait 13 seconds"  && sleep 5 \
+      && echo "wait 8 seconds"  && sleep 3 \
+      && echo "wait 5 seconds"  && sleep 2 \
+      && echo "wait 3 seconds"  && sleep 1 \
+      && echo "wait 2 seconds"  && sleep 1 \
+      && echo "wait 1 seconds"  && sleep 1
+  echo "データベース立ち上げ完了。Enterキーを2回押してください。その後、シェルをもう一度実行してください。"
+  echo ">SQL の入力画面にならなかった場合は、もう数秒待って再度シェルを実行してください。"
+  cd $bin_dir/../docker && docker-compose exec $container_name  sqlplus sys/oraoraora@localhost:1521/XE as sysdba  > /dev/null 2>&1
+
+fi
---
+++ tutorial/lesson/spring/docs/readme.md
@@ -0,0 +1,21 @@
+# スプリング環境テスト
+
+ ## データベース
+
+ oracle 11 を使用
+
+## .envファイル
+
+データベース用に環境変数の設定
+
+```env
+ORACLE_PWD=MyOraclePassword
+```
+
+ ## 参考
+
+[DockerでOracleデータベース11g XEを構築][*1]
+[dockerでOracle DB 11gを動かす][*2]
+
+[*1]:http://ryoichi0102.hatenablog.com/entry/2017/12/14/183046
+[*2]:http://tmegos.hatenablog.jp/entry/docker-oracle-11g
