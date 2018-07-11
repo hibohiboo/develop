cf2400dc9796a0b27a205abc9ee99ee4372cb871
hibohiboo <swdice66@gmail.com>
2018-07-12 03:09:17 +0900

    Hello world! spring!

--- tutorial/lesson/spring/src/hello/src/main/java/hello/hello/HelloApplication.java
+++ tutorial/lesson/spring/src/hello/src/main/java/hello/hello/HelloApplication.java
@@ -1,11 +1,17 @@
 package hello.hello;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloApplication {
 
+  @RequestMapping("/")
+  public String home() {
+      return "Hello World from Docker";
+  }
 	public static void main(String[] args) {
 		SpringApplication.run(HelloApplication.class, args);
 	}
