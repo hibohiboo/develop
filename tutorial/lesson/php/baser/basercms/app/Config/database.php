5beb80e74ce0bc740f44bee4c6305f859cb4386e
hibohiboo <swdice66@gmail.com>
2018-09-05 01:49:52 +0900

    cmsようにｇｄをインストール

---
+++ tutorial/lesson/php/baser/basercms/app/Config/database.php
@@ -0,0 +1,30 @@
+<?php
+//
+// Database Configuration File created by baserCMS Installation
+//
+class DATABASE_CONFIG {
+public $default = array(
+	'datasource' => '',
+	'persistent' => false,
+	'host' => 'localhost',
+	'port' => '',
+	'login' => 'dummy',
+	'password' => 'dummy',
+	'database' => 'dummy',
+	'schema' => '',
+	'prefix' => '',
+	'encoding' => 'utf8'
+);
+public $test = array(
+	'datasource' => '',
+	'persistent' => false,
+	'host' => 'localhost',
+	'port' => '',
+	'login' => 'dummy',
+	'password' => 'dummy',
+	'database' => 'dummy',
+	'schema' => '',
+	'prefix' => 'test_',
+	'encoding' => 'utf8'
+);
+}
