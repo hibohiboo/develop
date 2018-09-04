36f0275d2777fe4a03c7e6601d8127be27b9ea79
hibohiboo <swdice66@gmail.com>
2018-09-05 01:13:31 +0900

    開発環境準備

---
+++ tutorial/lesson/php/baser/bin/bash.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 引数を取得
+# db データベースコンテナ
+# otherdata データボリュームコンテナ：ログ
+# dbdata    データボリュームコンテナ：データベース
+# php7       PHP
+# nginx     サーバー
+container_name=${1:-db}
+
+# このシェルスクリプトのディレクトリの絶対パスを取得。
+bin_dir=$(cd $(dirname $0) && pwd)
+
+# $container_nameの有無をgrepで調べる
+docker ps | grep $container_name
+
+# grepの戻り値$?の評価。 grep戻り値 0:一致した 1:一致しなかった
+if [ $? -eq 0 ]; then
+  # 一致したときの処理
+  cd $bin_dir/../ && docker-compose exec $container_name bash
+else
+  # 一致しなかった時の処理
+  # コンテナを立ち上げて接続
+  cd $bin_dir/../ && docker-compose run $container_name /bin/bash
+fi
+
+
+
---
+++ tutorial/lesson/php/baser/src/index.php
@@ -0,0 +1,7 @@
+<?php
+  echo "test";
+  phpinfo();
+  echo "test";
+
+
+  
