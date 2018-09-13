cf061c89230e8bf6cb8de2d3c666783bfd9a62a6
hibohiboo <swdice66@gmail.com>
2018-09-13 23:25:57 +0900

    watchするwebpack構成を追加

---
+++ tutorial/lesson/elm/elm019webpack4/docker/elm/webpack.config.js
@@ -0,0 +1,182 @@
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const history = require('koa-connect-history-api-fallback');
+// const convert = require('koa-connect');
+// const proxy = require('http-proxy-middleware');
+
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+// to extract the css as a separate file
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+var MODE = process.env.npm_lifecycle_event === "prod" ? "production" : "development";
+var filename = MODE == "production" ? "[name]-[hash].js" : "index.js";
+
+var common = {
+    mode: MODE,
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "dist"),
+        // webpack -p automatically adds hash when building for production
+        filename: filename
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            // Use this template to get basic responsive meta tags
+            template: "src/index.html",
+            // inject details of output file at end of body
+            inject: "body"
+        })
+    ],
+    resolve: {
+        modules: [path.join(__dirname, "src"), "node_modules"],
+        extensions: [".js", ".elm", ".scss", ".png"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: ["style-loader", "css-loader", "sass-loader"]
+            },
+            {
+                test: /\.css$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: "url-loader",
+                options: {
+                    limit: 10000,
+                    mimetype: "application/font-woff"
+                }
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loader: "file-loader"
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader"
+            }
+        ]
+    },
+};
+
+if (MODE === "development") {
+    console.log("Building for dev...");
+    module.exports = merge(common, {
+      watch:true,
+      watchOptions: {
+        ignored: /node_modules/,
+        aggregateTimeout: 300,
+        // ５秒毎にポーリング
+        poll: 5000
+      },
+        plugins: [
+            // Suggested for hot-loading
+            new webpack.NamedModulesPlugin(),
+            // Prevents compilation errors causing the hot loader to lose state
+            new webpack.NoEmitOnErrorsPlugin()
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.elm$/,
+                    exclude: [/elm-stuff/, /node_modules/],
+                    use: [
+                        { loader: 'elm-hot-webpack-loader' },
+                        {
+                            loader: "elm-webpack-loader",
+                            options: {
+                                // add Elm's debug overlay to output
+                                debug: true,
+                                forceWatch: true,
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        serve: {
+            inline: true,
+            stats: "errors-only",
+            content: [path.join(__dirname, "src/assets")],
+            add: (app, middleware, options) => {
+                // routes /xyz -> /index.html
+                app.use(history());
+                // e.g.
+                // app.use(convert(proxy('/api', { target: 'http://localhost:5000' })));
+            },
+            hotClient:{
+              host: {
+                client: '192.168.50.10',
+                server: '0.0.0.0',
+              },
+                port:{
+                  server:3002,
+                  client: 3002
+                  }
+              },
+        },
+    });
+}
+
+if (MODE === "production") {
+    console.log("Building for Production...");
+    module.exports = merge(common, {
+        plugins: [
+            // Delete everything from output directory and report to user
+            new CleanWebpackPlugin(["dist"], {
+                root: __dirname,
+                exclude: [],
+                verbose: true,
+                dry: false
+            }),
+            new CopyWebpackPlugin([
+                {
+                    from: "src/assets"
+                }
+            ]),
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: "[name]-[hash].css"
+            })
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.elm$/,
+                    exclude: [/elm-stuff/, /node_modules/],
+                    use: [
+                        { loader: "elm-webpack-loader" }
+                    ]
+                },
+            {
+                test: /\.css$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                exclude: [/elm-stuff/, /node_modules/],
+                loaders: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
+            }
+            ]
+        }
+    });
+}
