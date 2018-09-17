3f7cac2a18a05ba66449e8041417dcc4f013b91d
hibohiboo <swdice66@gmail.com>
2018-09-18 00:52:00 +0900

    ビルド修正

--- tutorial/lesson/elm/elm-card/bin/build.sh
+++ tutorial/lesson/elm/elm-card/bin/build.sh
@@ -4,4 +4,4 @@
 bin_dir=$(cd $(dirname $0) && pwd)
 
 # docker-composeの起動。 コンテナ内に入る. 
-cd $bin_dir/../docker && docker-compose run elm yarn webpack
+cd $bin_dir/../docker && docker-compose run elm yarn prod
