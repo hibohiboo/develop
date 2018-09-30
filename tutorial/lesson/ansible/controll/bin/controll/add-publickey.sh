20800df43eba77639767167718bf3cf1e14f90b3
hibohiboo <swdice66@gmail.com>
2018-09-30 11:39:01 +0900

    ssh鍵をそれぞれ作成

--- tutorial/lesson/ansible/controll/bin/controll/add-publickey.sh
+++ tutorial/lesson/ansible/controll/bin/controll/add-publickey.sh
@@ -5,4 +5,4 @@ bin_dir=$(cd $(dirname $0) && pwd)
 
 # 192.168.74.60 の .ssh/authorized_keysに公開鍵を登録
 # ※公開鍵を置いているフォルダに秘密鍵もペアとなっていないと失敗する
-ssh-copy-id -o StrictHostKeyChecking=no -i $bin_dir/id_rsa.pub 192.168.74.60
+ssh-copy-id -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa.pub 192.168.74.60
