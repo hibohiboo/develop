04800b812c7b8b3f7fa706c5ecfe889efc2ac1dc
hibohiboo <swdice66@gmail.com>
2018-09-30 11:50:16 +0900

    ssh鍵の登録をbash化

--- tutorial/lesson/ansible/controll/bin/controll/add-publickey.sh
+++ tutorial/lesson/ansible/controll/bin/controll/add-publickey.sh
@@ -3,6 +3,9 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 
-# 192.168.74.60 の .ssh/authorized_keysに公開鍵を登録
+# ウェブサーバ（192.168.74.60） の .ssh/authorized_keysに公開鍵を登録
 # ※公開鍵を置いているフォルダに秘密鍵もペアとなっていないと失敗する
-ssh-copy-id -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa.pub 192.168.74.60
+sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa_web.pub 192.168.74.60
+
+# データベースサーバにも登録
+sshpass -p vagrant ssh-copy-id -o StrictHostKeyChecking=no -i /home/vagrant/.ssh/id_rsa_web.pub 192.168.7.25
--- tutorial/lesson/ansible/controll/bin/run.sh
+++ tutorial/lesson/ansible/controll/bin/run.sh
@@ -2,11 +2,18 @@
 # このシェルスクリプトのディレクトリの絶対パスを取得。
 bin_dir=$(cd $(dirname $0) && pwd)
 filename=$1
+private_key_path=$bin_dir/../.vagrant/machines/default/virtualbox/private_key
+
+if [[ $filename == "" ]]
+then
+    ssh 192.168.50.100 -l vagrant -i $private_key_path 
+    return
+fi
+
 
 if [[ $filename != *".sh" ]]
 then
     filename=`echo $1`.sh
 fi
-private_key_path=$bin_dir/../.vagrant/machines/default/virtualbox/private_key
-ssh 192.168.50.100 -l vagrant -i $private_key_path 
-#ssh 192.168.50.100 -l vagrant -i $private_key_path bash /vagrant/bin/controll/$filename
+
+ssh 192.168.50.100 -l vagrant -i $private_key_path bash /vagrant/bin/controll/$filename
