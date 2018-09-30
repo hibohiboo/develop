89f7b7a235cbd8af0f81fb19bfdcccb69edd099f
hibohiboo <swdice66@gmail.com>
2018-09-30 10:27:18 +0900

    expectsインストール

--- tutorial/lesson/ansible/controll/provision/bash/install_ansible.sh
+++ tutorial/lesson/ansible/controll/provision/bash/install_ansible.sh
@@ -2,7 +2,13 @@
 
 curl -kL "https://bootstrap.pypa.io/get-pip.py" | sudo python
 cd $HOME
+# pythonの仮想環境起動
 virtualenv venv
 source $HOME/venv/bin/activate
+
+# ansibleインストール
 pip install ansible
 ansible --version
+
+# ansible expectモジュールが使えるようにする
+pip install expects
