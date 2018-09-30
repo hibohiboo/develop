d955b1409f9ba7238bd13e8a762c246ef81300b8
hibohiboo <swdice66@gmail.com>
2018-09-30 11:11:50 +0900

    仮想環境でssh鍵を作る設定

--- tutorial/lesson/ansible/controll/provision/bash/install_ansible.sh
+++ tutorial/lesson/ansible/controll/provision/bash/install_ansible.sh
@@ -2,6 +2,10 @@
 
 curl -kL "https://bootstrap.pypa.io/get-pip.py" | sudo python
 cd $HOME
+
+# ansible expectモジュールが使えるようにする(ansibleの仮想環境ではなくローカルに必要)
+sudo pip install pexpect
+
 # pythonの仮想環境起動
 virtualenv venv
 source $HOME/venv/bin/activate
@@ -10,5 +14,3 @@ source $HOME/venv/bin/activate
 pip install ansible
 ansible --version
 
-# ansible expectモジュールが使えるようにする
-pip install expects
