cf1c059e86c8fb464cf96f3aed1c85a82f2a7d2c
hibohiboo <swdice66@gmail.com>
2018-10-02 08:08:16 +0900

    外部から秘密鍵を渡した場合についての処理を追加

---
+++ tutorial/lesson/ansible/controll/bin/controll/ssh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# ssh 192.168.74.60 -p 22 -l vagrant -i /home/vagrant/.ssh/secret.key 
+ssh 192.168.74.60 -p 22 -l vagrant -i /home/vagrant/.ssh/id_rsa_web 
--- tutorial/lesson/ansible/controll/bin/run.sh
+++ tutorial/lesson/ansible/controll/bin/run.sh
@@ -3,17 +3,27 @@
 bin_dir=$(cd $(dirname $0) && pwd)
 filename=$1
 private_key_path=$bin_dir/../.vagrant/machines/default/virtualbox/private_key
+target_ip=192.168.50.100 
 
+# 引数なしの場合はコントロールサーバのvagrantにログイン
 if [[ $filename == "" ]]
 then
-    ssh 192.168.50.100 -l vagrant -i $private_key_path 
+    ssh $target_ip -l vagrant -i $private_key_path 
     exit
 fi
-
 
 if [[ $filename != *".sh" ]]
 then
     filename=`echo $1`.sh
 fi
 
-ssh 192.168.50.100 -l vagrant -i $private_key_path bash /vagrant/bin/controll/$filename
+# sshの場合は-t で疑似端末をつける
+if [[ $filename == "ssh"* ]]
+then
+    # https://qiita.com/moroku0519/items/62686784a51ad54b6ac7
+    ssh $target_ip -t -l vagrant -i $private_key_path bash /vagrant/bin/controll/$filename
+    exit
+fi
+
+# その他の場合はcontrollフォルダ内のshellファイルをcontrollサーバに接続して実行
+ssh $target_ip -l vagrant -i $private_key_path bash /vagrant/bin/controll/$filename
