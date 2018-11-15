c750f3a4d1ce521851c2d5ad6bef02b9f6d37606
hibohiboo <swdice66@gmail.com>
2018-11-15 23:04:12 +0900

    scssに変更

--- tutorial/lesson/webpack/webpack4/bin/sass/lint.sh
+++ tutorial/lesson/webpack/webpack4/bin/sass/lint.sh
@@ -5,4 +5,4 @@ bin_dir=$(cd $(dirname $0) && pwd)
 name=${1:-webpacks}
 dir_docker="$bin_dir/../../docker"
 
-cd $dir_docker  && docker-compose run $name yarn run stylelint /app/src/assets/css/*.sass --custom-syntax postcss-scss
+cd $dir_docker  && docker-compose run $name yarn run stylelint /app/src/assets/css/*.scss
