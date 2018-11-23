ad674a2cd6f94f795dc8ccbb5097dcb036a23195
hibohiboo <swdice66@gmail.com>
2018-11-23 23:09:35 +0900

    docについて追加

--- tutorial/lesson/webpack/webpack-separate/bin/ts/doc.sh
+++ tutorial/lesson/webpack/webpack-separate/bin/ts/doc.sh
@@ -5,4 +5,4 @@ bin_dir=$(cd $(dirname $0) && pwd)
 name=${1:-ts}
 dir_docker="$bin_dir/../../docker"
 # docker-composeの起動。 
-cd $dir_docker  && docker-compose run $name yarn run  typedoc --target es6  --ignoreCompilerErrors --out ./dist/docs/ts ./src/
+cd $dir_docker  && docker-compose run $name yarn run  typedoc --target es6  --ignoreCompilerErrors --out ./docs/ts ./src/
