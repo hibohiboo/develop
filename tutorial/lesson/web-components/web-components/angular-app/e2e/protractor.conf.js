0398f2d3c9fe7ee687bb4d980522c9a12868dc3e
hibohiboo <swdice66@gmail.com>
2019-05-09 01:48:51 +0900

    テストが成功するように修正

--- tutorial/lesson/web-components/web-components/angular-app/e2e/protractor.conf.js
+++ tutorial/lesson/web-components/web-components/angular-app/e2e/protractor.conf.js
@@ -9,7 +9,11 @@ exports.config = {
     './src/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chrome'
+    browserName: 'chrome',
+    chromeOptions: {
+      binary: process.env.CHROME_BIN,
+      args: ['--headless', '--disable-gpu', '--no-sandbox']
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -17,7 +21,7 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
