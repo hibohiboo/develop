e883455a2876878afc542108ddb792664025a849
hibohiboo <swdice66@gmail.com>
2019-05-09 22:12:43 +0900

    テストを成功するように修正

--- tutorial/lesson/web-components/web-components/angular-app/src/app/hello/hello.component.spec.ts
+++ tutorial/lesson/web-components/web-components/angular-app/src/app/hello/hello.component.spec.ts
@@ -1,29 +1,29 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
 
-describe('AppComponent', () => {
+describe('HelloComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        HelloComponent
       ],
     }).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'angular-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-app');
   });
 
   it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HelloComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('div').textContent).toContain('angular-app');
