e6fee7b8bc3b501f0caceddf04a451426a39c7fc
hibohiboo <swdice66@gmail.com>
2019-05-11 23:35:52 +0900

    ansible install

--- tutorial/lesson/dotnet/dotnetcore22/provision/bash/install_ansible.sh
+++ tutorial/lesson/dotnet/dotnetcore22/provision/bash/install_ansible.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# echo $PATHは以下の結果となり、/usr/local/binは含まれていない。
-# /usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin
-/usr/local/bin/python3.7 -V
-
-# curl -kL "https://bootstrap.pypa.io/get-pip.py" | sudo python
-# cd $HOME
-# virtualenv venv
-# source $HOME/venv/bin/activate
-# pip install ansible
-# ansible --version
+curl -kL "https://bootstrap.pypa.io/get-pip.py" | sudo /usr/local/bin/python3.7
+cd $HOME
+/usr/local/bin/virtualenv venv
+source $HOME/venv/bin/activate
+pip install ansible
+ansible --version
