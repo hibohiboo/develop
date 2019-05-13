c16071b7bb41479132e081a4bb1612fbddb7d1ac
hibohiboo <swdice66@gmail.com>
2019-05-14 00:37:26 +0900

    ディレクトリを移動

---
+++ tutorial/lesson/sqlserver/docker/provision/bash/install_ansible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -kL "https://bootstrap.pypa.io/get-pip.py" | sudo /usr/local/bin/python3.7
+cd $HOME
+/usr/local/bin/virtualenv venv
+source $HOME/venv/bin/activate
+pip install ansible
+ansible --version
+pip install docker
