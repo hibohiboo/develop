b3e1502fdb41fb9818c853fed8c2098a5620ccff
hibohiboo <swdice66@gmail.com>
2019-05-14 23:34:51 +0900

    Mvcスキャフォールディング追加時点

--- tutorial/lesson/dotnet/dotnetcore22/MvcMovie/MvcMovie/Startup.cs
+++ tutorial/lesson/dotnet/dotnetcore22/MvcMovie/MvcMovie/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -33,6 +35,9 @@ namespace MvcMovie
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
