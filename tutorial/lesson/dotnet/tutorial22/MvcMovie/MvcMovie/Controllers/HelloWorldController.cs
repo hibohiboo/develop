5fdc4adb33efb05a90f522024adb92ab9eb81421
hibohiboo <swdice66@gmail.com>
2019-05-18 23:39:24 +0900

    書き換え時点

--- tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -1,16 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MvcMovie.Controllers
 {
     public class HelloWorldController : Controller
     {
-        public IActionResult Index()
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
         {
-            return View();
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
         }
     }
 }
