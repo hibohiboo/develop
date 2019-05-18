3bda4665e69ed6856592263a7edda56781403618
hibohiboo <swdice66@gmail.com>
2019-05-19 00:12:57 +0900

    モデルの位置修正

--- tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Controllers/HomeController.cs
+++ tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using MvcMovie.Models;
+using MvcMovie.Core.Models;
 
 namespace MvcMovie.Controllers
 {
--- tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Core/Models/ErrorViewModel.cs
+++ tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Core/Models/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace MvcMovie.Models
+namespace MvcMovie.Core.Models
 {
     public class ErrorViewModel
     {
