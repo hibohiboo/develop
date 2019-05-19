e86e3fc92302ab1a98754c0fef188b78941c9bea
hibohiboo <swdice66@gmail.com>
2019-05-19 11:52:12 +0900

    Identityのスキャフォールディング

---
+++ tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
+
+[assembly: HostingStartup(typeof(MvcMovie.Areas.Identity.IdentityHostingStartup))]
+namespace MvcMovie.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<MvcMovieIdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("MvcMovieIdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<MvcMovieIdentityContext>();
+            });
+        }
+    }
+}
