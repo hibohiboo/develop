33fa1c49ddddbc83f979387e23d5329d60330b92
hibohiboo <swdice66@gmail.com>
2019-05-19 11:58:15 +0900

    トップページに認証のpartialページを追加.承認機能を有効にする。

--- tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Startup.cs
+++ tutorial/lesson/dotnet/tutorial22/MvcMovie/MvcMovie/Startup.cs
@@ -55,6 +55,7 @@ namespace MvcMovie
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
             app.UseMvc(routes =>
