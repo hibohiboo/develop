58f7eb1685c22cbb97a78ae977bcf8f5f22fc217
hibohiboo <swdice66@gmail.com>
2019-10-06 08:53:06 +0900

    jsonを返すAPI化

--- tutorial/lesson/vscode/api-server/src/app.ts
+++ tutorial/lesson/vscode/api-server/src/app.ts
@@ -1,7 +1,25 @@
 import * as Express from 'express';
 const app = Express();
 app.get('/', (req, res) => {
-  res.send('Hello, VS Code!!!');
+  res.send('Hello, VS Code!!');
 });
+
+interface Task {
+  category: string;
+  title: string;
+  done: boolean;
+}
+
+const tasks: Task[] = [
+  {
+    category: "Private",
+    title: "買い物",
+    done: false
+  }
+]
+
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+})
 
 export { app };
