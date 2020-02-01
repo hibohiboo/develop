207add48d60763e5cda1209b56343f3930061d96
hibohiboo <swdice66@gmail.com>
2020-02-01 22:21:24 +0900

    流れを確認

--- tutorial/lesson/react-my-examples/my-react-ts/src/index.tsx
+++ tutorial/lesson/react-my-examples/my-react-ts/src/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { configureStore } from '@reduxjs/toolkit';
 import App from "./components/App";
+import reducer from './reducers';
+import { addTodo } from "./actions";
 
-// reactのコンポーネントを#root以下に作成する
+const store = configureStore({ reducer });
+store.dispatch(addTodo("Hello World!"));
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById("root"));
--- tutorial/lesson/react-my-examples/my-react-ts/src/reducers/index.ts
+++ tutorial/lesson/react-my-examples/my-react-ts/src/reducers/index.ts
@@ -5,15 +5,8 @@ function initialState() {
   return { id: 0, text: '' };
 }
 
-export const reducer = createReducer(initialState(), builder =>
+const reducer = createReducer(initialState(), builder =>
   builder
     .addCase(addTodo, (state, action) => (action.payload))
 );
-
-// const init = initialState();
-// const addTodo = addTodoCreator(init.text);
-// export const reducer = createReducer(init, {
-//   [addTodo.type]: (state, action) => {
-//     return action.payload;
-//   },
-// });
+export default reducer;
