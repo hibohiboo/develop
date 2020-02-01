590cd4413cfbed5f2392eae6f9127e6746009900
hibohiboo <swdice66@gmail.com>
2020-02-02 02:58:59 +0900

    クリックで切替

--- tutorial/lesson/react-my-examples/my-react-ts/src/components/Todo.tsx
+++ tutorial/lesson/react-my-examples/my-react-ts/src/components/Todo.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Todo: React.FC<{ completed: boolean, text: string, }> = ({ completed, text }) => {
-  return <li style={{ textDecoration: completed ? 'line-through' : 'none' }}>
+const Todo: React.FC<{ completed: boolean, text: string, toggle: any }> = React.memo(({ completed, text, toggle }) => {
+  return <li style={{ textDecoration: completed ? 'line-through' : 'none' }} onClick={toggle}>
     {text}
-  </li>;
-};
+  </li >;
+});
 
 export default Todo;
--- tutorial/lesson/react-my-examples/my-react-ts/src/components/TodoList.tsx
+++ tutorial/lesson/react-my-examples/my-react-ts/src/components/TodoList.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { toggleTodo } from "../actions";
 import Todo from "./Todo";
 import { useTodoItems } from '../reducers/todos';
 
 const TodoList: React.FC = props => {
   const todos = useTodoItems();
+  const dispatch = useDispatch();
+  const toggle = useCallback((id) => dispatch(toggleTodo(id)), [dispatch]);
   return (
     <ul>
       {todos.map(todo => (
-        <Todo key={todo.id} {...todo} />
+        <Todo key={todo.id} {...todo} toggle={() => toggle(todo.id)} />
       ))}
     </ul>
   );
