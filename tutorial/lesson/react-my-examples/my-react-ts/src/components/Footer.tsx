b5bbd9428b3f4fa50258826dfadee5f70f12e770
hibohiboo <swdice66@gmail.com>
2020-02-03 01:20:10 +0900

    activeな状態のリンクを押せないようにする

--- tutorial/lesson/react-my-examples/my-react-ts/src/components/Footer.tsx
+++ tutorial/lesson/react-my-examples/my-react-ts/src/components/Footer.tsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import Link from './Link';
-import visibilityFilterModules from '../module/visibilityFilterModules';
+import visibilityFilterModules, { useVisibleFilter } from '../module/visibilityFilterModules';
 
 const Footer: React.FC = () => {
   const dispatch = useDispatch();
   const visibilityFilter = visibilityFilterModules.actions.visibilityFilter;
+  const filter = useVisibleFilter();
 
   return <p>
     Show:
   {" "}
-    <Link onClick={() => dispatch(visibilityFilter('SHOW_ALL'))}>
+    <Link onClick={() => dispatch(visibilityFilter('SHOW_ALL'))} active={filter === 'SHOW_ALL'}>
       All
   </Link>
     {", "}
-    <Link onClick={() => dispatch(visibilityFilter('SHOW_ACTIVE'))}>
+    <Link onClick={() => dispatch(visibilityFilter('SHOW_ACTIVE'))} active={filter === 'SHOW_ACTIVE'}>
       Active
   </Link>
     {", "}
-    <Link onClick={() => dispatch(visibilityFilter('SHOW_COMPLETED'))}>
+    <Link onClick={() => dispatch(visibilityFilter('SHOW_COMPLETED'))} active={filter === 'SHOW_COMPLETED'}>
       Completed
   </Link>
   </p>;
--- tutorial/lesson/react-my-examples/my-react-ts/src/components/Link.tsx
+++ tutorial/lesson/react-my-examples/my-react-ts/src/components/Link.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 
-const Link: React.FC<{ children: any, onClick: () => void }> = ({ children, onClick }) => {
+const Link: React.FC<{ active: boolean, children: any, onClick: () => void }> = ({ active, children, onClick }) => {
+  if (active) {
+    return <span>{children}</span>;
+  }
+  // eslint-disable-next-line jsx-a11y/anchor-is-valid
   return <a href="#" onClick={(e) => { e.preventDefault(); onClick(); }}>{children}</a>;
 };
 
