f18bd6541819e39f7c4c62b5891e5ec3facc589f
hibohiboo <swdice66@gmail.com>
2020-03-08 12:00:02 +0900

    確認

--- tutorial/lesson/cssGuide/docs/readme.md
+++ tutorial/lesson/cssGuide/docs/readme.md
@@ -5,4 +5,6 @@
 ## 参考
 
 [storybook for htmlがあまりにも使いづらいのでpugを使って上手く利用する方法を考えてみた](https://qiita.com/BigFly3/items/1e9bf8c0a3f786556bb7)
+[静的なWEBサイト用にも使えるstorybookのpug環境を作ってみた](https://qiita.com/BigFly3/items/81b2a7c75c143b63ebde)
+[上記サイトの参考githubリポジトリ](https://github.com/BigFly3/storybook_html_pugdev)
 [Storybook for HTML](https://storybook.js.org/docs/guides/guide-html/)
--- tutorial/lesson/cssGuide/stories/header/header.stories.js
+++ tutorial/lesson/cssGuide/stories/header/header.stories.js
@@ -2,10 +2,13 @@ import { storiesOf } from '@storybook/html';
 import copyCodeBlock from '@pickra/copy-code-block';
 import Pug2Html from '!html-loader!pug-plain-loader!./header.pug'
 import PugSrc from '!html-loader!./header.pug'
-import Css from '!style-loader!css-loader!./header.css'
 import CssSrc from '!raw-loader!./header.css'
-
+import '!style-loader!css-loader!./header.css'
+import hljs from 'highlight.js/lib/highlight';
+hljs.registerLanguage('css', require('highlight.js/lib/languages/css'));
+// pugは対応していなかった。
+hljs.registerLanguage('pug', require('highlight.js/lib/languages/haml'));
 
 storiesOf('header', module)
-  .add('pug2html', () => Css + Pug2Html) //htmlタグに変換されたものを表示するだけ
-  .add('pug&html', () => Css + Pug2Html + copyCodeBlock(PugSrc) + copyCodeBlock(CssSrc))
+  .add('pug2html', () => Pug2Html) //htmlタグに変換されたものを表示するだけ
+  .add('pug&html', () => Pug2Html + copyCodeBlock(PugSrc, { lang: 'pug' }) + copyCodeBlock(CssSrc, { lang: 'css' }))
