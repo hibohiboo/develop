bce44745ffd2071736cc5308c94b23e413a4471d
hibohiboo <swdice66@gmail.com>
2020-11-04 01:44:12 +0900

    fix 型

--- tutorial/lesson/ms-functions/ms-functions-connect-samples/SqlExample/models/users.ts
+++ tutorial/lesson/ms-functions/ms-functions-connect-samples/SqlExample/models/users.ts
@@ -1,7 +1,10 @@
 import * as db from './db';
-
+interface User {
+  UserID: number
+  Name: string
+}
 export const select = async ()=>{
   const connection = await db.getConnection();
   const query = "select * from Users";
-  return await db.read(connection, query)
+  return await db.read<User[]>(connection, query)
 }
