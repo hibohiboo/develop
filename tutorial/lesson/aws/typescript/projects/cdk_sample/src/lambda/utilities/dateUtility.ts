7e96d3e289220f8f39aebf778d94cc75220f790e
hibohiboo <swdice66@gmail.com>
2021-03-18 02:52:41 +0900

    日本時間に変更するサンプル

--- tutorial/lesson/aws/typescript/projects/cdk_sample/src/lambda/utilities/dateUtility.ts
+++ tutorial/lesson/aws/typescript/projects/cdk_sample/src/lambda/utilities/dateUtility.ts
@@ -1,4 +1,5 @@
 import { format, parseISO as parse } from 'date-fns'
 
 export const formatISO = (d: Date) => format(d, "yyyy-MM-dd'T'HH:mm:ss")
+export const formatISOUTC = (d: Date) => format(d, "yyyy-MM-dd'T'HH:mm:ss'Z'")
 export const parseISO = (s: string) => parse(s)
--- tutorial/lesson/aws/typescript/projects/cdk_sample/test/utilities/dateUtility.test.ts
+++ tutorial/lesson/aws/typescript/projects/cdk_sample/test/utilities/dateUtility.test.ts
@@ -1,10 +1,25 @@
 import * as dateUtility from '@/lambda/utilities/dateUtility'
+import { addHours } from 'date-fns'
 
 describe('format', () => {
   test('日時が正しい場合', () => {
     const date = dateUtility.parseISO('2000-01-01')
     const x = dateUtility.formatISO(date)
     expect(x).toEqual('2000-01-01T00:00:00')
+  })
+  test('zonedTimeToUtc', () => {
+    // UTC環境だと "1999-12-31T15:00:00Z"になってしまう
+    // const date = zonedTimeToUtc('2000-01-01', 'Asia/Tokyo')
+    const date = dateUtility.parseISO('2000-01-01')
+    const x = dateUtility.formatISOUTC(date)
+    // formatISO(date)だと、ローカルでは"2000-01-01T00:00:00+09:00"になってしまう
+    expect(x).toEqual('2000-01-01T00:00:00Z')
+  })
+  test('日本時間に変更して表示', () => {
+    const date = dateUtility.parseISO('2000-01-01')
+    const x = dateUtility.formatISO(addHours(date, 9))
+
+    expect(x).toEqual('2000-01-01T09:00:00')
   })
 })
 
